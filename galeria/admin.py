dbf7b515b21486a429b5922f96210faa75e41b81
William Wsilva <williamreis.wsilva@gmail.com>
2023-11-08 19:32:19 -0300

    config models

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'categoria', 'descricao') # campos exibidos na tabela
+    list_display_links = ('id', 'nome') # campos clicaveis para editar
+    search_fields = ('nome',) # campo de busca pelo campo nome
+    list_filter = ('categoria',)
+
+admin.site.register(Fotografia, ListandoFotografias)
