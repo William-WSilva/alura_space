12f026c78d7ce49a823482f71bfbc2559bb7e17a
William Wsilva <williamreis.wsilva@gmail.com>
2023-11-08 21:15:53 -0300

    configurando Campo de busca, arquivo models e urls

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda', 'categoria', 'descricao') # campos exibidos na tabela
+    list_display = ('id', 'nome', 'legenda', 'categoria', 'descricao', 'publicada') # campos exibidos na tabela
     list_display_links = ('id', 'nome') # campos clicaveis para editar
     search_fields = ('nome',) # campo de busca pelo campo nome
-    list_filter = ('categoria',)
+    list_filter = ('categoria',) # filtro rápido por categoria
+    list_editable = ('publicada',)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
 
@@ -6,14 +7,17 @@ class Fotografia(models.Model):
         ('NEBULOSA', 'Nebulosa'),
         ('ESTRELA', 'Estrela'),
         ('GALAXIA', 'Galaxia'),
-        ('PLANETA', 'Planeta')
+        ('PLANETA', 'Planeta'),
+        ('CORPO_CELESTE', 'Corpo_Celeste')
     ]
 
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='-')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
