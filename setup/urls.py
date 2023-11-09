b60a555768fca0045bc8630e9865b097b3db32c6
William Wsilva <williamreis.wsilva@gmail.com>
2023-11-09 10:53:21 -0300

    Criando novo app usuarios: configurando formulários de login e cadastro

--- setup/urls.py
+++ setup/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('galeria.urls')) # Rota do projeto galeria
+    path('', include('galeria.urls')), # Rota do projeto galeria
+    path('', include('usuarios.urls')) # Rota do projeto usuarios
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
