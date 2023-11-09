c34632d35672b779eda9f2b6cf5bbf0b6e987041
William Wsilva <williamreis.wsilva@gmail.com>
2023-11-09 16:18:03 -0300

    configurando e validando formulario de cadastro e login

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,7 +5,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ('id', 'nome', 'legenda', 'categoria', 'descricao', 'publicada') # campos exibidos na tabela
     list_display_links = ('id', 'nome') # campos clicaveis para editar
     search_fields = ('nome',) # campo de busca pelo campo nome
-    list_filter = ('categoria',) # filtro rápido por categoria
+    list_filter = ('categoria', 'usuario',) # filtro rápido por categoria
     list_editable = ('publicada',)
     list_per_page = 10
 
