bd043141f2d39ba3636d58d7a14a3cc1b8c59c7a
William Wsilva <williamreis.wsilva@gmail.com>
2023-11-09 22:59:09 -0300

    configurações credenciais AWS e proteção de credênciais concluídas

--- setup/urls.py
+++ setup/urls.py
@@ -6,5 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('apps.galeria.urls')), # Rota do projeto galeria
-    path('', include('apps.usuarios.urls')) # Rota do projeto usuarios
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('apps.usuarios.urls')), # Rota do projeto usuarios
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
